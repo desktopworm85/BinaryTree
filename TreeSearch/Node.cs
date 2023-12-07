278c96f76e53b9718ffe6a05535839c724569270
desktopworm <39500730+desktopworm85@users.noreply.github.com>
2023-12-07 12:00:31 +0000

    Update Node.cs

--- TreeSearch/Node.cs
+++ TreeSearch/Node.cs
@@ -5,11 +5,15 @@ namespace TreeSearch {
         public int value;
         public Node? left;
         public Node? right;
+        public Node? parent;
+        public bool visited;
 
         public Node(int data) {
             value = data;
             left = null;
             right = null;
+            parent = null;
+            visited = false;
         }
 
         public void AddNode(Node root) {
@@ -19,18 +23,20 @@ namespace TreeSearch {
             } else if(root.value == value) {
                 Console.WriteLine("Cannot add duplicate value");
                 return;
-            } else if (root.value < value) {
+            } else if (value < root.value) {
                 if (root.left != null) {
                     AddNode(root.left);
                 } else {
                     Console.WriteLine("Adding " + value + " left of " + root.value);
+                    parent = root;
                     root.left = this;
                 }
-            } else if (root.value > value) {
+            } else if (value > root.value) {
                 if (root.right != null) {
                     AddNode(root.right);
                 } else {
                     Console.WriteLine("Adding "+ value + " right of " + root.value);
+                    parent = root;
                     root.right = this;
                 }
             }
